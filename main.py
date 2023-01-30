40ca8fbb327fc1de8004a0f64e916df3b3e4486a
pilarorellana <110754794+pilarorellana@users.noreply.github.com>
2023-01-29 21:43:08 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,18 @@
+# import the custom module
+import triangleArea
+
+'''
+def triangulation(base, height):
+    area = (1/2)*(base)*(height)
+    return area
+'''
+
+x = 10 # base
+y = 20 # height
+
+
+# ^ this is what the 
+# module looks like 
+# module_name.function_name
+
+print(triangleArea.triangulation(x,y))
---
+++ triangleArea.py
@@ -0,0 +1,9 @@
+# In this note we create
+# a module for main.py.
+# Recall: a module is a python
+# document leveraged for 
+# main.py.
+
+def triangulation(base, height):
+    area = (1/2)*(base)*(height)
+    return area
